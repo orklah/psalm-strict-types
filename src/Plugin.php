cca57586b8c4375509413fb46d08b731c013bf9e
orklah <ronan_morin@hotmail.com>
2021-01-21 21:25:12 +0100

    Add strict_types in my classes. Oh the irony...

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 namespace Orklah\StrictTypes;
 
 use Orklah\StrictTypes\Hooks\StrictTypesHooks;
--- tests/Analyzers/NonStrictCode/NonStrictMethodCallTest.php
+++ tests/Analyzers/NonStrictCode/NonStrictMethodCallTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Orklah\StrictTypes\Tests\Analyzers\InvalidCode;
 
