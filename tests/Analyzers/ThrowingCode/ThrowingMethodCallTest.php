f85d07d2d3ce28c31c8c27c2e67922940fdb93b1
orklah <ronan_morin@hotmail.com>
2021-01-24 00:20:06 +0100

    messed up namespace

--- tests/Analyzers/ThrowingCode/ThrowingMethodCallTest.php
+++ tests/Analyzers/ThrowingCode/ThrowingMethodCallTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Orklah\StrictTypes\Tests\Analyzers\StrictDeclarationCode;
+namespace Orklah\StrictTypes\Tests\Analyzers\ThrowingCode;
 
 use Orklah\StrictTypes\Tests\Internal\ThrowingTestCase;
 use Psalm\Context;
