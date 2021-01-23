c942c304c060193a22bed4830ac4aa4e838f9074
orklah <ronan_morin@hotmail.com>
2021-01-23 22:02:31 +0100

    throwingTest that doesn't work yet

---
+++ tests/Analyzers/ThrowingCode/ThrowingMethodCallTest.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Orklah\StrictTypes\Tests\Analyzers\StrictDeclarationCode;
+
+use Orklah\StrictTypes\Tests\Internal\ThrowingTestCase;
+use Psalm\Context;
+
+class ThrowingMethodCallTest extends ThrowingTestCase
+{
+    /**
+     * This needs to be fixed, when in a namespace and outside the class, no node provider holds the variable?!?
+     */
+    public function testMethodParamStrictInNamespace(): void
+    {
+        $this->addFile(
+            __CLASS__.__METHOD__.'.php',
+            '<?php
+            namespace bar;
+            class A{
+                public function foo(string $a): void {}
+            }
+
+            $a = new A();
+            $a->foo("");'
+        );
+
+        $this->analyzeFile(__CLASS__.__METHOD__.'.php', new Context());
+    }
+
+    /**
+     * This needs to be fixed, when in a namespace and outside the class, no node provider holds the variable?!?
+     */
+    public function testNamespacedMethodParamStrictInNamespace(): void
+    {
+        $this->addFile(
+            __CLASS__.__METHOD__.'.php',
+            '<?php
+            namespace bar;
+            class A{
+                public function foo(string $a): void {}
+            }
+
+            $a = new \bar\A();
+            $a->foo("");'
+        );
+
+        $this->analyzeFile(__CLASS__.__METHOD__.'.php', new Context());
+    }
+
+    /**
+     * This needs to be fixed, it implies being able to calculate the target of static
+     */
+    public function testStaticParamStatic(): void
+    {
+        $this->addFile(
+            __CLASS__.__METHOD__.'.php',
+            '<?php
+            class A{
+                public static function test(int $a){}
+            }
+
+            class B extends A{
+                public static function test(int $a){
+                    static::test(1);
+                }
+            }'
+        );
+
+        $this->analyzeFile(__CLASS__.__METHOD__.'.php', new Context());
+    }
+}
--- tests/Internal/ThrowingTestCase.php
+++ tests/Internal/ThrowingTestCase.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 namespace Orklah\StrictTypes\Tests\Internal;
 
 use Orklah\StrictTypes\Exceptions\ShouldNotHappenException;
+use Psalm\Exception\CodeException;
 
-abstract class ThrowingTestCase extends BaseTestCase
+abstract class ThrowingTestCase /*extends BaseTestCase*/ //this doesn't work yet
 {
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->expectException(ShouldNotHappenException::class);
+        $this->expectException(CodeException::class);
+        $this->expectExceptionMessage('ShouldNotHappenException');
     }
 }
