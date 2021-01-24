e23de9dd80f5b7e6c67c19d4ec5de5815cebb31a
orklah <ronan_morin@hotmail.com>
2021-01-24 10:46:21 +0100

    fix tests

--- tests/Analyzers/NonStrictCode/NonStrictAssignTest.php
+++ tests/Analyzers/NonStrictCode/NonStrictAssignTest.php
@@ -46,7 +46,7 @@ class NonStrictAssignTest extends NonStrictTestCase
             __CLASS__.__METHOD__.'.php',
             '<?php
             class A{
-                public string $a;
+                public static string $a;
 
                 public function __construct(){
                     self::$a = 1;
@@ -63,11 +63,11 @@ class NonStrictAssignTest extends NonStrictTestCase
             __CLASS__.__METHOD__.'.php',
             '<?php
             class A{
-                public string $a;
+                public static string $a;
             }
 
             $a = new A();
-            $a::$a = 1;'
+            $a::$b = 1;'
         );
 
         $this->analyzeFile(__CLASS__.__METHOD__.'.php', new Context());
